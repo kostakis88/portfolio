ba5804c76758751332478089ef6db8428a4d0d7b
Kostas Ntallas <kostasdallas2010@hotmail.com>
2019-06-02 16:30:04 +0300

    added portfolio items vie resource generator

---
+++ db/migrate/20190602132750_create_highlights.rb
@@ -0,0 +1,13 @@
+class CreateHighlights < ActiveRecord::Migration[5.2]
+  def change
+    create_table :highlights do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
