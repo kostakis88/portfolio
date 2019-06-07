86c7fee32176e08490d615903f43e45506f3bece
Kostas Ntallas <kostasdallas2010@hotmail.com>
2019-06-08 00:09:34 +0300

    integrated new and create actions for portfolios

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -2,4 +2,20 @@ class HighlightsController < ApplicationController
     def index
         @highlights = Highlight.all
     end
+
+    def new
+        @highlight = Highlight.new
+    end
+
+    def create
+        @highlight = Highlight.new(params.require(:highlight).permit(:title, :subtitle, :body))
+    
+        respond_to do |format|
+          if @highlight.save
+            format.html { redirect_to highlights_path, notice: 'Highlight was successfully created.' }
+          else
+            format.html { render :new }
+          end
+        end
+    end
 end
