78a4cd4d0323e1be7373d37408bf6f2d3cde70bc
Kostas Ntallas <kostasdallas2010@hotmail.com>
2019-06-10 23:04:05 +0300

    override resources route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :highlights
+  resources :highlights, except: [:show]
+  get 'highlights/:id', to: 'highlights#show', as: 'highlight_show'
+
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   resources :blogs
