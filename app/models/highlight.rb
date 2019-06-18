f041f9b427c55712b37476b0bf9cec6b5ee18fb3
Kostas Ntallas <kostasdallas2010@hotmail.com>
2019-06-18 19:54:11 +0300

    implemented custom scopes for portfolio items

--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -1,3 +1,9 @@
 class Highlight < ApplicationRecord
     validates_presence_of :title, :body, :main_image, :thumb_image
+
+    def self.reactjs
+        where(subtitle: 'React JS')
+    end
+
+    scope :ruby_on_rails_highlight_items, -> { where(subtitle: 'Ruby on Rails') }
 end
