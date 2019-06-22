d28312e3036c0b0947f2d1656a10d85905714004
Kostas Ntallas <kostasdallas2010@hotmail.com>
2019-06-22 17:30:06 +0300

    customized registration, login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :highlights, except: [:show]
   get 'highlights/:id', to: 'highlights#show', as: 'highlight_show'
 
