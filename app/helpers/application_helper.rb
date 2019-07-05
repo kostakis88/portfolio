108f3be39afba52fd28f4b38dd5d0f1016c14118
Kostas Ntallas <kostasdallas2010@hotmail.com>
2019-07-05 22:47:20 +0300

    integrated new copyright gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
             content_tag(:p, greeting, class: "source-greeting")
         end 
     end
+
+    def copyright_generator
+        @copyright = HighlightsViewTool::Renderer.copyright 'Kostas Ntallas', 'All rights reserved'
+    end
 end
