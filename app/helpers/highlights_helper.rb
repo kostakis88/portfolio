baffa2440c5ece1c9dc0548e7e20b59463032e2f
Kostas Ntallas <kostasdallas2010@hotmail.com>
2019-07-21 12:40:13 +0300

    updated upload forms

--- app/helpers/highlights_helper.rb
+++ app/helpers/highlights_helper.rb
@@ -1,2 +1,14 @@
 module HighlightsHelper
+    def image_generator(height:, width:)
+        "https://via.placeholder.com/#{height}x#{width}"
+    end
+    def highlight_img img, type
+        if img
+            img
+        elsif type == 'thumb'
+            image_generator(height: '350', width: '200')
+        elsif type == 'main'
+            image_generator(height: '600', width: '400')
+        end
+    end    
 end
