ea0ff4ceb00baa461cd279c392d71feb907ba8c1
Kostas Ntallas <kostasdallas2010@hotmail.com>
2019-08-05 20:34:58 +0300

    created a comment broadcast, websocket route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :highlights, except: [:show] do
     put :sort, on: :collection
@@ -15,5 +14,7 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ActionCable.server => '/cable' 
+
   root to: 'pages#home'
 end
